53bc3aacd6fc9dc8cf9f0e700f32e083e21f3532
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-03-24 19:36:35 -0300

    Adicionado arquivos javascript

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+const age = 20;
+//variável constante não pode ter o valor alterado.
+
+//== compara valores
+
+
+//=== compara valores e tipo
+
+
+let name = "kikuti";
+console.log(age);
+console.log(name);
